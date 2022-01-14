b61c992ff66bf3c88a80bdb8cb7e8e63341af944
Yuxian Xu <yuxia@Mac-mini.local>
2022-01-13 21:17:19 -0800

    Create activity buttons

--- client/src/pages/Activity/activity.js
+++ client/src/pages/Activity/activity.js
@@ -1,34 +1,38 @@
-import React, { Component } from 'react';
-import axios from 'axios';
-import { Link } from 'react-router-dom';
-import './Activity.scss'
+import React, { Component } from "react";
+import axios from "axios";
+import { Link } from "react-router-dom";
+import "./Activity.scss";
 
 export default class Activity extends Component {
-    constructor() {
-        super();
-        this.state = {
-            activityData: null,
-        };
-    }
-
-    getActivityData = () => {
-        axios
-        .get()
-        .then((res) => {
-            this.setState( { activityData: res.data })
-        })
-        .catch((e) => {
-            console.error(e);
-            alert('something went wrong');
-        });
+  constructor() {
+    super();
+    this.state = {
+      activityData: [],
     };
+  }
 
+  componentDidMount() {
+    axios
+      .get(`http://localhost:8080/activity`)
+      .then((res) => {
+        this.setState({ activityData: res.data });
+      })
+      .catch((e) => {
+        console.error(e);
+        alert("something went wrong");
+      });
+  }
 
-    render() {
-        return (
-            <div>
-                Activity page
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div className="activity">
+        <div>Activity page -1</div>
+        <div>
+          {this.state.activityData.map((element) => {
+            return <button className="activity__button">{element}</button>;
+          })}
+        </div>
+      </div>
+    );
+  }
 }
